e4539d00fb32d89c3238d0dc07a4b3d674ffa1f0
thesmartwon <thesmartwon@live.com>
2019-04-13 16:48:25 -0400

    Clean app.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,17 +2,19 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 exports.onCreateWebpackConfig = ({ getConfig, stage, rules, loaders, plugins, actions }) => {
   const config = getConfig();
-  // console.log('stage', stage);
+  console.log('config', config);
   if (stage === 'build-javascript') {
-    // Use our custom entry
+    // Use our custom entrypoint
     config.entry.app = './src/production-app.js';
+    // Disable sourcemaps: https://webpack.js.org/configuration/devtool/
+    config.devtool = false;
 
     config.plugins.unshift(new CleanWebpackPlugin({
       // https://github.com/sindresorhus/del#patterns
       cleanOnceBeforeBuildPatterns: ['**/*', '!**/static/**']
     }));
   } else if (stage === 'develop') {
-    // Use our custom entry
+    // Use our custom entrypoint
     const commons = config.entry.commons.filter(a => a.indexOf('.cache/app') === -1);
     commons.push('./src/app.js');
     config.entry.commons = commons;
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,3 +1,3 @@
 import React from "react"
 
-export default () => <div>Page 2!</div>
+export default () => <div>Page 2 woohoo!</div>
